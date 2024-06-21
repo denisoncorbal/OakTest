c75c79987a56bc34441cd5470c690c4faf7bfca7
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2024-06-21 10:57:57 -0300

    feat(backend): base spring boot project

---
+++ backend/src/main/java/com/dgc/oaktest/OakTestApplication.java
@@ -0,0 +1,13 @@
+package com.dgc.oaktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakTestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dgc/oaktest/OakTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgc.oaktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OakTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
